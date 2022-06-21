a421e72b05bde7f3e79e3dfe28e8701ad1ffa86a
Adrian <adrianwojciak99@gmail.com>
2022-06-21 15:36:02 +0200

    init example

---
+++ example/src/App.tsx
@@ -0,0 +1,16 @@
+import Button from "@mui/material/Button";
+import "./App.css";
+
+function App() {
+  return (
+    <div id="nightly-anchor" className="App">
+      <header className="App-header">
+        <Button variant="contained" style={{ margin: 10 }}>
+          open modal
+        </Button>
+      </header>
+    </div>
+  );
+}
+
+export default App;
