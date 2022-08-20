0e412e8fd48943f82da78c728cc1461e64d9bfef
Adrian <adrianwojciak@interia.pl>
2022-08-20 15:29:25 +0200

    fix example

--- example/src/App.tsx
+++ example/src/App.tsx
@@ -11,6 +11,7 @@ import "./App.css";
 import { NightlyWalletAdapter } from "@nightlylabs/solana-adapter";
 import { sign } from "tweetnacl";
 import bs58 from "bs58";
+import { sha256 } from 'js-sha256'
 
 const NightlySolana = new NightlyWalletAdapter();
 const connection = new Connection("https://api.devnet.solana.com");
@@ -104,10 +105,11 @@ function App() {
               }
 
               const message = new TextEncoder().encode("Hello world!");
+              const shaMessage = Uint8Array.from(sha256.array("Hello world!"));
               const signature = await NightlySolana.signMessage(message);
 
               // Verify that the bytes were signed using the private key that matches the known public key
-              if (!sign.detached.verify(message, signature, pk.toBytes()))
+              if (!sign.detached.verify(shaMessage, signature, pk.toBytes()))
                 throw new Error("Invalid signature!");
 
               alert(`Message signature: ${bs58.encode(signature)}`);
